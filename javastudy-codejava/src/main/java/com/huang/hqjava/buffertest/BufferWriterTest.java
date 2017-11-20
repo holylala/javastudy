096ea731d224cb316932c8f091bc6bb4cdc1bee2
huangzhenqiong <huangzhenqiong@sina.cn>
2017-11-20 16:45:22 +0800

    add buffer method

---
+++ javastudy-codejava/src/main/java/com/huang/hqjava/buffertest/BufferWriterTest.java
@@ -0,0 +1,48 @@
+package com.huang.hqjava.buffertest;
+
+import java.io.*;
+
+/**
+ * @author zhenqionghuang@creditease.cn
+ * @version V1.0
+ * @Package com.huang.hqjava.buffertest
+ * @Description:
+ * @date 2017/11/17 18:11
+ * @Company: creditease.cn
+ */
+public class BufferWriterTest {
+
+	public static void main(String[] args) throws IOException {
+		bufferTest();
+	}
+
+	private static void bufferTest() throws IOException {
+
+		Writer writer = new FileWriter(new File("file.txt"));
+
+		long begin = System.currentTimeMillis();
+
+		for (int i = 0; i < 100000; i++) {
+			writer.write(i);
+		}
+
+		writer.close();
+		//129ms
+		System.out.println("testFileWriter spend:"+(System.currentTimeMillis() - begin));
+
+		//使用缓冲
+
+		Writer bufferWriter = new BufferedWriter(new FileWriter(new File("file.txt")));
+
+		begin = System.currentTimeMillis();
+
+		for (int i = 0; i < 100000; i++) {
+			bufferWriter.write(i);
+		}
+
+		writer.close();
+		//24ms
+		System.out.println("testBufferFileWriter spend:"+(System.currentTimeMillis() - begin));
+
+	}
+}
