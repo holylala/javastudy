838a4a27c4dafcf113b42b1cb07f2e5750d4987a
huangzhenqion <huangzhenqiong@sina.cn>
2017-12-20 23:36:39 +0800

    buffer test

--- javastudy-codejava/src/main/java/com/huang/hqjava/buffertest/MyBuffer.java
+++ javastudy-codejava/src/main/java/com/huang/hqjava/buffertest/MyBuffer.java
@@ -19,9 +19,43 @@ public class MyBuffer {
 	private static void whatIsBuffer() {
 
 		ByteBuffer b = ByteBuffer.allocate(15);
-		System.out.println("初始\tlimit="+b.limit()+"\tcapacity="+b.capacity()+"\tposition="+b.position());
+		System.out.println("初始值\tlimit="+b.limit()+"\tcapacity="+b.capacity()+"\tposition="+b.position());
 
+		//存入10个字节数据
+		for (int i = 0; i < 10; i++) {
+			b.put((byte)i);
+		}
 
+		System.out.println("存入10个字节数据之后\tlimit="+b.limit()+"\tcapacity="+b.capacity()+"\tposition="+b.position());
+
+		//重置position
+		b.flip();
+
+		System.out.println("Buffer重置之后\tlimit="+b.limit()+"\tcapacity="+b.capacity()+"\tposition="+b.position());
+
+		for (int i = 0; i < 5; i++) {
+			System.out.print(b.get());
+		}
+
+		System.out.println();
+
+		System.out.println("Buffer取5个值之后\tlimit="+b.limit()+"\tcapacity="+b.capacity()+"\tposition="+b.position());
+
+		b.flip();
+
+		System.out.println("Buffer再次重置之后\tlimit="+b.limit()+"\tcapacity="+b.capacity()+"\tposition="+b.position());
+
+//		for (int i = 0; i < 5; i++) {
+//			System.out.print(b.get());
+//		}
+//
+//		System.out.println("Buffer再次读之后\tlimit="+b.limit()+"\tcapacity="+b.capacity()+"\tposition="+b.position());
+//		b.flip();
+//		for (int i = 0; i < 4; i++) {
+//			b.put((byte)i);
+//		}
+//
+//		System.out.println("Buffer再次设值之后\tlimit="+b.limit()+"\tcapacity="+b.capacity()+"\tposition="+b.position());
 	}
 
 }
