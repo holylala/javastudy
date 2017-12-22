fa821ca3c6f3e783c3abf73bd9aa1f2b7718063a
huangzhenqiong <huangzhenqiong@sina.cn>
2017-12-22 18:45:41 +0800

    add mark

---
+++ javastudy-codejava/src/main/java/com/huang/optimize/concurrency/Future/RealData.java
@@ -0,0 +1,24 @@
+package com.huang.optimize.concurrency.Future;
+
+import java.util.concurrent.Callable;
+
+/**
+ * @author zhenqionghuang@creditease.cn
+ * @version V1.0
+ * @Package com.huang.optimize.concurrency.Future
+ * @Description:
+ * @date 2017/12/21 18:28
+ * @Company: creditease.cn
+ */
+public class RealData implements Callable<String> {
+	/**
+	 * Computes a result, or throws an exception if unable to do so.
+	 *
+	 * @return computed result
+	 * @throws Exception if unable to compute a result
+	 */
+	@Override
+	public String call() throws Exception {
+		return null;
+	}
+}
