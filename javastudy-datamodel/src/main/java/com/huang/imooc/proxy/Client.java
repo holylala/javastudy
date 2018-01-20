dee62a5989cc0eced38a8b80b025b907d78ef673
huangzhenqion <huangzhenqiong@sina.cn>
2018-01-20 23:07:44 +0800

    代理模块

--- javastudy-datamodel/src/main/java/com/huang/imooc/proxy/Client.java
+++ javastudy-datamodel/src/main/java/com/huang/imooc/proxy/Client.java
@@ -14,9 +14,16 @@ public class Client {
 //		Moveable m = new TimeCounter1();
 //		m.move();
 		//使用聚合方式实现
+//		Car car = new Car();
+//		Moveable m1 = new TimeCounter2(car);
+//		m1.move();
+
+		//聚合方式实现代理功能的叠加
+
 		Car car = new Car();
-		Moveable m1 = new TimeCounter2(car);
-		m1.move();
+		CarLogProxy clp = new CarLogProxy(car);
+		CarTimeProxy ctp = new CarTimeProxy(clp);
+		ctp.move();
 	}
 
 }
