5518064b47415b465d04f8c4b4553c43a847b5de
huangzhenqion <huangzhenqiong@sina.cn>
2018-02-04 23:14:05 +0800

    单例模式用例

--- javastudy-datamodel/src/main/java/com/huang/imooc/singleton/RunMain.java
+++ javastudy-datamodel/src/main/java/com/huang/imooc/singleton/RunMain.java
@@ -19,5 +19,14 @@ public class RunMain {
             System.out.println("s1和s2不是同一个实例");
         }
 
+        //懒汉模式
+        Singleton2 s3=Singleton2.getInstance();
+        Singleton2 s4=Singleton2.getInstance();
+        if(s3==s4){
+            System.out.println("s3和s4是同一个实例");
+        }else{
+            System.out.println("S3和s4不是同一个实例");
+        }
+
     }
 }
