5f5988f6ae415bcf8e3d926bf3f2de1dcd156d0e
huangzhenqion <huangzhenqiong@sina.cn>
2019-03-21 23:23:07 +0800

    add some thread sample classes

---
+++ javastudy-codejava/src/main/java/com/threadesignbook/StopThreadUnsafe.java
@@ -0,0 +1,116 @@
+package com.threadesignbook;
+
+/**
+ * @author huangzhenqiong@sina.cn
+ * @version V1.0
+ * @Package com.threadesignbook
+ * @Description:
+ * @date 2019/3/17 23:11
+ * @Company:
+ */
+public class StopThreadUnsafe {
+
+    public static User u = new User();
+
+    public static class User {
+        private int id;
+        private String name;
+        public User() {
+            id = 0;
+            name = "0";
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return "User{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    '}';
+        }
+    }
+
+    public static class ChangeObjectThread extends Thread {
+
+        //增加自行决定退出线程的方法
+        volatile boolean stopme = false;
+
+        public void stopMe() {
+            stopme = true;
+        }
+
+        @Override
+        public String toString() {
+            stopMe();
+            return super.toString();
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+
+                if (stopme) {
+                    System.out.println("exit by stop me");
+                    break;
+                }
+
+                synchronized (u) {
+                    int v = (int) (System.currentTimeMillis()/1000);
+                    u.setId(v);
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    u.setName(String.valueOf(v));
+                }
+                Thread.yield();
+            }
+        }
+    }
+
+
+    public static class ReadObjectThread extends Thread {
+
+        @Override
+        public void run() {
+            while (true) {
+                synchronized (u) {
+                    if (u.getId() != Integer.parseInt(u.getName())) {
+                        System.out.println(u.toString());
+                    }
+                }
+                Thread.yield();
+            }
+        }
+
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        new ReadObjectThread().start();
+        while (true) {
+            Thread t = new ChangeObjectThread();
+            t.start();
+            Thread.sleep(150);
+           // t.stop();//强制终止线程 对象一致性无法保证
+            t.toString();
+        }
+    }
+
+
+}
---
+++ javastudy-codejava/src/main/java/com/threadesignbook/ThreadInterruptTest.java
@@ -0,0 +1,40 @@
+package com.threadesignbook;
+
+/**
+ * @author huangzhenqiong@sina.cn
+ * @version V1.0
+ * @Package com.threadesignbook
+ * @Description:
+ * @date 2019/3/21 23:11
+ * @Company:
+ */
+public class ThreadInterruptTest {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread() {
+
+            @Override
+            public void run() {
+                while(true) {
+                    if (Thread.currentThread().isInterrupted()) {
+                        System.out.println("CurrentThread Interrupted!");
+                        break;
+                    }
+                    try {
+                        Thread.sleep(2000);//sleep方法 中断会抛出异常 此时会清楚中断标记
+                    } catch (InterruptedException e) {
+                        System.out.println("Interrupted When Sleep");
+                        //再次设置中断标记
+                        Thread.currentThread().interrupt();
+                    }
+                    Thread.yield();
+                }
+            }
+
+        };
+        t1.start();
+        Thread.sleep(1000);
+        t1.interrupt();//通知目标线程中断 并设置中断标志位
+    }
+
+}
